3575009e125476e144b4e3214a00fe58ab08f1cc
mindovermatter <39469932+MindOverDevelopment@users.noreply.github.com>
2023-11-23 10:00:13 -0500

    Tree VFXs and touch
    Touch to fire a tree

---
+++ Assets/Scripts/Seed.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seed : MonoBehaviour
+{
+    [SerializeField] private GameObject tree;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(tree, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    public GameObject projectilePrefab; // Assign your projectile prefab in the Unity Editor
+    public float projectileForce = 10f;
+    public bool isActive;
+
+    void Update()
+    {
+        // Check if there is any touch input
+        if (Input.touchCount > 0)
+        {
+            // Get the first touch
+            Touch touch = Input.GetTouch(0);
+
+            // Check if the touch phase is the beginning of a touch
+            if (touch.phase == TouchPhase.Began)
+            {
+                // Call the method to fire the projectile
+                FireProjectile(touch.position);
+            }
+        }
+    }
+
+    void FireProjectile(Vector2 touchPosition)
+    {
+        if (!isActive)
+        {
+            // Convert the touch position to a world point
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(touchPosition.x, touchPosition.y, Camera.main.nearClipPlane));
+
+            // Instantiate the projectile prefab at the touch position
+            GameObject projectile = Instantiate(projectilePrefab, worldPosition, Quaternion.identity);
+
+            // Get the Rigidbody component of the projectile
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+            // Check if the Rigidbody component is not null
+            if (rb != null)
+            {
+                // Calculate the direction away from the camera
+                Vector3 direction = (worldPosition - Camera.main.transform.position).normalized;
+
+                // Apply force to the projectile in the calculated direction
+                rb.AddForce(direction * projectileForce, ForceMode.Impulse);
+
+                isActive = true;
+            }
+        }
+
+    }
+}
